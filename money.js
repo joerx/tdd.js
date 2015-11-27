96bc6ad65d068b93801a30cae4655a86d44c9511
Joerg Henning <henning.joerg@gmail.com>
2015-11-27 14:02:29 +0530

    Revised classes. Turns out we don't need them.
    "Favor object composition
    over class inheritance."

--- money.js
+++ money.js
@@ -2,67 +2,79 @@
 
 let assert = require('assert')
 
-class Wallet extends Array {
-  plus(money) {
-    this.push(money)
-    return this
+// this doesn't look as smart as it used to be. think about something better...
+function wallet() {
+  let wallet = Array.prototype.slice.call(arguments)
+
+  wallet.plus = function(money) {
+    wallet.push(money)
+    return wallet
   }
+
+  return wallet 
 }
 
-class Money {
-  constructor(amount, currency) {
-    this.amount = amount
-    this.currency = currency
+function bank() {
+  let rates = new Map()
+
+  let self = {
+    reduce: function(wallet, currency) {
+      return wallet.reduce((prev, cur) => {
+        if (!prev) return prev
+        else return money(prev.amount + self.convert(cur, currency).amount, currency)
+      })
+    },
+
+    rate: function(from, to, value) {
+      var key = from + ':' + to
+      if (value) rates.set(key, value)
+      return rates.get(key)
+    },
+
+    convert: function(someMoney, toCurrency) {
+      if (someMoney.currency === toCurrency) return someMoney
+      var rate = self.rate(someMoney.currency, toCurrency)
+      assert(rate, `rate ${someMoney.currency} to ${toCurrency} is missing`)
+      return money(someMoney.amount * rate, toCurrency)
+    }
   }
 
-  times(by) {
-    return new this.constructor(this.amount * by, this.currency)
-  }
-
-  plus(money) {
-    return new Wallet(this, money)
-  }
-
-  equal(other) {
-    return this.currency === other.currency 
-      && (this.amount === other.amount)
-  }
+  return self
 }
 
-class Bank {
-  constructor() {
-    this.rates = new Map()
+function money(amount, currency) {
+  let self = {
+    times: function (by) {
+      return money(amount * by, currency)
+    },
+
+    plus: function(money) {
+      return wallet(self, money)
+    },
+
+    equal: function(other) {
+      return currency === other.currency 
+        && (amount === other.amount)
+    },
+
+    get amount() {
+      return amount
+    },
+
+    get currency() {
+      return currency
+    }
   }
 
-  reduce(wallet, currency) {
-    return wallet.reduce((prev, cur) => {
-      if (!prev) return prev
-      else return new Money(prev.amount + this.convert(cur, currency).amount, currency)
-    })
-  }
-
-  rate(from, to, value) {
-    var key = from + ':' + to
-    if (value) this.rates.set(key, value)
-    return this.rates.get(key)
-  }
-
-  convert(money, toCurrency) {
-    if (money.currency === toCurrency) return money
-    var rate = this.rate(money.currency, toCurrency)
-    assert(rate, `rate ${money.currency} to ${toCurrency} is missing`)
-    return new Money(money.amount * rate, toCurrency)
-  }
+  return self
 }
 
 module.exports.usd = function usd(amount) {
-  return new Money(amount, 'USD')
+  return money(amount, 'USD')
 }
 
 module.exports.sgd = function sgd(amount) {
-  return new Money(amount, 'SGD')
+  return money(amount, 'SGD')
 }
 
-module.exports.bank = function() {
-  return new Bank()
-}
+module.exports.bank = bank
